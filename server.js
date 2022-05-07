60a71da1199a2490a08008958bc8cfb0ce701ada
CassDoes <c.doexo@gmail.com>
2022-05-07 02:19:35 -0500

    attempting to alter addEmployee function to an async funciton

--- server.js
+++ server.js
@@ -106,11 +106,12 @@ async function displayEmployees() {
     CONCAT (employee.last_name,', ',employee.first_name) AS Employee,
     role.title AS Position,
     department.name AS Department,
-    role.salary AS Salary
+    role.salary AS Salary,
+    CONCAT (manager.last_name,', ',manager.first_name) AS Manager
     FROM employee
     LEFT JOIN role ON employee.role_id = role.id
     LEFT JOIN department ON role.department_id = department.id
-    ORDER BY employee.last_name
+    LEFT JOIN employee manager ON employee.manager_id = manager.id
   `);
   console.table(rows);
   startPrompt();
@@ -143,59 +144,90 @@ const addDepartment = () => {
     )
 };
 
+//ADD a ROLE
+
+
 //ADD an EMPLOYEE
-const addEmployee = () => {
-  return inquirer.prompt ([
-    {
-      type: 'input',
-      name: 'first_name',
-      message: "Please enter the first name of the employee.",
-      validate: firstName => {
-        if (firstName) {
-          return true;
-        } else {
-          return false;
-        }
-      }      
-    },
-    {
-      type: 'input',
-      name: 'last_name',
-      message: "Please enter the last name of the employee.",
-      validate: lastName => {
-        if (lastName) {
-          return true;
-        } else {
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'role',
-      message: "What is the employee's role?",
-    },
-    {
-      type: 'input',
-      name: 'manager_name',
-      message: "Who is the employee's manager?",
-    },
-  ])
+async function addEmployee() {
+  const roles = await findRoles();
+  const reportTo = await findManagers();
+
+  // `SELECT title, id, FROM roles;
+  // `
+  
+  
+  // let newEmployeeRole = `
+  //   SELECT role.title AS title, 
+  //   role.id AS roleID,
+  //   employee.id AS employeeID,
+  //   CONCAT (manager.last_name,', ',manager.first_name) AS manager
+  //   FROM employee
+  //   LEFT JOIN role ON employee.role_id = role.id
+  //   LEFT JOIN employee manager ON employee.manager_id = manager.id;
+  //   `
+
+  //let newEmployeeManager = `
+    // SELECT manager.first_name, manager.last_name, id AS iD
+    // FROM employee
+    // WHERE manager_id IS NULL;`
+
+    // db.promise().query(newEmployeeRole)
+    //   .then( ([rows]) => {
+        const position = roles.map(({ title, id }) => ({ name: title, value: id }))
+        const managerName = reportTo.map(({ manager, employeeID }) => ({ name: manager, value: employeeID }))
+      return inquirer.prompt ([
+        {
+          type: 'input',
+          name: 'first',
+          message: "Please enter the first name of the employee.",
+          validate: firstName => {
+            if (firstName) {
+              return true;
+            } else {
+              return false;
+            }
+          }      
+        },
+        {
+          type: 'input',
+          name: 'last',
+          message: "Please enter the last name of the employee.",
+          validate: lastName => {
+            if (lastName) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        },
+        {
+          type: 'list',
+          name: 'role',
+          message: "What is the employee's role?",
+          choices: position
+        },
+        {
+          type: 'list',
+          name: 'manager',
+          message: "Who is the employee's manager?",
+          choices: managerName
+        },
+      ])
+
 
   .then(employeeData => {
     const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES (?,?,?,?)`;
-    const param = employeeData.first_name;
-    const params = employeeData.last_name;
+    const params = [employeeData.first, employeeData.last, employeeData.role, employeeData.manager];
     
-    db.promise().query(sql, param, params, (err, result) => {
+    db.promise().query(sql, params, (err, result) => {
       if (err) {
         console.log(err);
       }
     })
-    console.log('Added ' + employeeData.first_name + ' to the COMPANY database');
+    console.log('Added ' + employeeData.first + ' to the COMPANY database');
   })
-  .then(startPrompt);
+    .then(startPrompt);
 };
 
 // class DB{
